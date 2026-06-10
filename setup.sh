c5d10ddf2f390a4d6d8f473a3c4192c8ecb655f6
Amani5576 <smmnjoroge@gmail.com>
2026-06-10 21:25:18 +0200

    removing git in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,3 @@ dir=$(dirname $BASH_SOURCE)/processMeerKAT
 export PATH=$dir:$PATH
 export PYTHONPATH=$dir:$PYTHONPATH
 export SINGULARITYENV_PYTHONPATH="$PYTHONPATH:\$PYTHONPATH"
-git config --global --add safe.directory $(dirname $BASH_SOURCE)
